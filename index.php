81269a3e4d334ced0dc229a85255cd800e117e55
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-09-28 02:14:35 -0400

    5 string functions in total

--- index.php
+++ index.php
@@ -18,7 +18,25 @@
    $text = "<hr><br>";
    stringFunctions::printThis($text);
 
-   }
+   $text = "<h1>THIS IS STRTOUPPER FUNCTION</h1><br>";
+   stringFunctions::printThis($text);
+   stringFunctions::strtoupperF($text);
+   $text = "<hr><br>";
+   stringFunctions::printThis($text);
+
+   $text = "<h1>THIS IS UCFIRST FUNCTION</h1><br>";
+   stringFunctions::printThis($text);
+   stringFunctions::ucfirstF($text);
+   $text = "<hr><br>";
+   stringFunctions::printThis($text);
+
+   $text = "<h1>THIS IS STRWORDCOUNT FUNCTION</h1><br>";
+   stringFunctions::printThis($text);
+   stringFunctions::str_word_countF($text);
+   $text = "<hr><br>";
+   stringFunctions::printThis($text);
+
+}
 
   public function __destruct() {
    $text = "end of my program<br>";
@@ -40,6 +58,18 @@
      $text = "this line is in lower<br>";
      print($text);
      print(strtoupper($text));
+     }
+     static public function ucfirstF($text) {
+     $text = "first character will become uppercase";
+     print($text);
+     echo "<br>";
+     print(ucfirst($text));
+     }
+     static public function str_word_countF($text) {
+     $text = "count words in this line";
+     print($text);
+     echo "<br>";
+     print(str_word_count($text));
      }
   }
 
